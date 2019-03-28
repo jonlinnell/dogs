99061e301fd3b5269cccc17cf97047c03263aaa2
Jon Linnell <linnell.jon@gmail.com>
2019-03-28 08:01:52 +0000

    Lint. Check for configuration before rendering app

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,5 +1,5 @@
 import 'babel-polyfill';
-import React, { useContext } from 'react';
+import React from 'react';
 import { render } from 'react-dom';
 import { ThemeProvider } from 'styled-components';
 import { Router } from '@reach/router';
@@ -7,7 +7,7 @@ import { Router } from '@reach/router';
 import Main from './pages/Main';
 
 import GlobalStyles from './helpers/GlobalStyle';
-import authContext, { AuthProvider } from './helpers/authContext';
+import { AuthProvider } from './helpers/authContext';
 import theme from './theme.json';
 
 const isIE = () => {
@@ -18,20 +18,29 @@ const isIE = () => {
   return msie > 0 || trident > 0;
 };
 
-const App = () => {
-  return (
-    <ThemeProvider theme={theme}>
-      <React.Fragment>
-        <GlobalStyles />
-        <AuthProvider>
-          <Router>
-            <Main path="/" />
-          </Router>
-        </AuthProvider>
-      </React.Fragment>
-    </ThemeProvider>
-  );
-};
+const App = () => (
+  <ThemeProvider theme={theme}>
+    <React.Fragment>
+      <GlobalStyles />
+      {
+        process.env.API // Has the app been configured?
+        ? (
+          <AuthProvider>
+            <Router>
+              <Main path="/" />
+            </Router>
+          </AuthProvider>
+        )
+        : (
+          <div style={{ height: '100vh', textAlign: 'center', margin: '20vh 10vw' }}>
+            <h1>App not configured.</h1>
+            <p><span style={{ fontFamily: 'Courier New'}}>process.env.API</span> isn't set. Set this environment variable and reload/rebuild the app.</p>
+          </div>
+        )
+      }
+    </React.Fragment>
+  </ThemeProvider>
+);
 
 if (isIE()) {
   const message =
